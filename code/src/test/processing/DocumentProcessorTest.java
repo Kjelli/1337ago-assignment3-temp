348906a6081091d1db2caa168ad1203d447ac180
Kjell Arne Sandvold Hellum <kjehe91@gmail.com>
2015-10-25 16:49:12 +0100

    atomized tests for separation of concerns

--- code/src/test/processing/DocumentProcessorTest.java
+++ code/src/test/processing/DocumentProcessorTest.java
@@ -6,6 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Map.Entry;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -15,48 +16,64 @@ import documents.UnprocessedDocument;
 
 public class DocumentProcessorTest {
 
-	@Test
-	public void sampleDocumentProcessingTest() {
+	Document doc;
+	UnprocessedDocument updoc;
+
+	@Before
+	public void setUp() {
 		try {
-			// Sample html file to read
 			String html = new String(Files.readAllBytes(Paths
 					.get("bitesize/sample000.html")), StandardCharsets.UTF_8);
-			UnprocessedDocument updoc = new UnprocessedDocument("sample", html);
+			updoc = new UnprocessedDocument("sample", html);
 
-			// Processing the sample
-			Document doc = DocumentProcessor.process(updoc);
-
-			assertNotNull("Document should exist", doc);
-
-			assertSame("Name of the document should persist", updoc.getName(),
-					doc.getName());
-
-			assertNotNull("Term-frequency map should exist",
-					doc.getOccurences());
-
-			assertTrue("Term-frequency map should have entries", doc
-					.getOccurences().size() > 0);
-
-			assertTrue("Term-frequency map should not contain stopwords", doc
-					.getOccurences().get("is") == null);
-
-			for (Entry<String, Integer> entry : doc.getOccurences().entrySet()) {
-				assertTrue(
-						"Entries im the term-frequency map should be lowercase",
-						entry.getKey().toLowerCase().equals(entry.getKey()));
-				assertTrue(
-						"Entries in the term-frequency map should not be empty",
-						!entry.getKey().isEmpty());
-				assertTrue(
-						"Entries in the term-frequency should have non-zero positive values",
-						entry.getValue() > 0);
-				// Print occurences for visual confirmation
-				System.out.println(entry);
-			}
-
+			doc = DocumentProcessor.process(updoc);
 		} catch (IOException e) {
 			throw new AssertionError(
 					"Test failed; could not read sample html file");
 		}
+	}
+
+	@Test
+	public void existenceTest() {
+		assertNotNull("Document should exist", doc);
+	}
+
+	@Test
+	public void namePersistenceTest() {
+		assertSame("Name of the document should persist", updoc.getName(),
+				doc.getName());
+	}
+
+	@Test
+	public void termFrequencyMapExistenceTest() {
+		assertNotNull("Term-frequency map should exist", doc.getOccurences());
+	}
+
+	@Test
+	public void termFrequencyMapNotEmptyTest() {
+		assertTrue("Term-frequency map should have entries", doc
+				.getOccurences().size() > 0);
+	}
+
+	@Test
+	public void termFrequencyMapStopwordTest() {
+
+		assertTrue("Term-frequency map should not contain stopwords", doc
+				.getOccurences().get("is") == null);
+	}
+
+	@Test
+	public void termFormatTest() {
+
+		for (Entry<String, Integer> entry : doc.getOccurences().entrySet()) {
+			assertTrue("Entries im the term-frequency map should be lowercase",
+					entry.getKey().toLowerCase().equals(entry.getKey()));
+			assertTrue("Entries in the term-frequency map should not be empty",
+					!entry.getKey().isEmpty());
+			assertTrue(
+					"Entries in the term-frequency should have non-zero positive values",
+					entry.getValue() > 0);
+		}
+
 	}
 }
