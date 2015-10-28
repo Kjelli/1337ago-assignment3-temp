6a9705f8c4cc98f20321dbb0e5438539e76f2222
Aurega <m.he@stud.uis.no>
2015-10-28 17:00:17 +0100

    updated queryprocessor

--- code/src/processing/QueryProcessor.java
+++ code/src/processing/QueryProcessor.java
@@ -4,18 +4,22 @@ import queries.Query;
 
 public class QueryProcessor {
 	private static String regex = "[ \\\\\\-\\\n\r\"\t<>+*”“’&©{}\\[\\]%~#=$•·—– ,.?!'|();:/]+| ";
+
 	public QueryProcessor(){};
-	
-	//makes the unprocessed query into a processed one
-	public static Query process(UnprocessedQuery rawQuery){
+
+	// makes the unprocessed query into a processed one
+	public static Query process(UnprocessedQuery rawQuery) {
 		TermProcessor termProc = TermProcessor.getInstance();
 		String[] terms = rawQuery.queries.split(regex);
+		String tempTerm;
 		Query query = new Query();
-		for (int i = 0; i < terms.length-1; i++){
-			if (i == 3) query.id = Integer.parseInt(terms[i]);
+		for (int i = 0; i < terms.length - 1; i++) {
+			if (i == 3)
+				query.id = Integer.parseInt(terms[i]);
 			if (i > 3) {
-				if (!termProc.isStopWord(terms[i])){
-				query.terms.add(terms[i]);
+				tempTerm = termProc.process(terms[i]);
+				if (!tempTerm.equals("")) {
+					query.terms.add(tempTerm);
 				}
 			}
 		}
